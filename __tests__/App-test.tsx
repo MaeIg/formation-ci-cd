a382086a66386aec71befa058b80712da0a8d93e
MaeIg <larochmael@hotmail.fr>
2022-12-07 09:53:35 +0100

    test: mock react-native-codepush

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -9,6 +9,30 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('react-native-code-push', () => {
+  // @ts-ignore
+  const cp = () => app => app;
+  Object.assign(cp, {
+    InstallMode: {},
+    CheckFrequency: {},
+    SyncStatus: {},
+    UpdateState: {},
+    DeploymentStatus: {},
+    DEFAULT_UPDATE_DIALOG: {},
+
+    allowRestart: jest.fn(),
+    checkForUpdate: jest.fn(() => Promise.resolve(null)),
+    disallowRestart: jest.fn(),
+    getCurrentPackage: jest.fn(() => Promise.resolve(null)),
+    getUpdateMetadata: jest.fn(() => Promise.resolve(null)),
+    notifyAppReady: jest.fn(() => Promise.resolve()),
+    restartApp: jest.fn(),
+    sync: jest.fn(() => Promise.resolve(1)),
+    clearUpdates: jest.fn(),
+  });
+  return cp;
+});
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
